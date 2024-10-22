a2c0cd37ddc49e67e327490076bd03b0208e91d0
Phonso <abdoulazizc867@gmail.com>
2024-10-22 13:25:56 +0000

    Turtle exercise : Many squares!

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import turtle
 
-def square():
+def square(size):
     for i in range(0, 4):
         t.left(90)
-        t.forward(100)
+        t.forward(size)
+
+
+def squares(init_size, nb):
+    for i in range(0, nb):
+        size = (i + 1) * nb
+        square(size)
 
 
 t = turtle.Turtle()
-square()
+squares(500, 15)
 turtle.done()
